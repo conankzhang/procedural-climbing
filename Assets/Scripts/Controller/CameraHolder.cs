b56e6387f0fe3fb6785c870135841b566bf14eb8
conankzhang <conankzhang@gmail.com>
2018-08-25 13:15:11 -0600

    CameraHolder: Create simple camera follow

---
+++ Assets/Scripts/Controller/CameraHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralClimbing
+{
+    public class CameraHolder : MonoBehaviour {
+
+        public Transform target;
+        public float speed = 9;
+
+        
+        // Update is called once per frame
+        void FixedUpdate () {
+           if(target == null)
+            {
+                return;
+            }
+
+            Vector3 p = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+            transform.position = p;
+        }
+    }
+}
