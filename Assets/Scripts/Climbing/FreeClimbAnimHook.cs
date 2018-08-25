85ab54852b02eab3d436b1879d2d6515a9492dc6
conankzhang <conankzhang@gmail.com>
2018-08-25 12:51:57 -0600

    FreeClimbAnimHook: Set up bisected movement

--- Assets/Scripts/Climbing/FreeClimbAnimHook.cs
+++ Assets/Scripts/Climbing/FreeClimbAnimHook.cs
@@ -10,6 +10,7 @@ namespace ProceduralClimbing
         IKSnapshot ikBase;
         IKSnapshot current = new IKSnapshot();
         IKSnapshot next = new IKSnapshot();
+        IKGoals goals = new IKGoals();
 
         public float weight_rh;
         public float weight_lh;
@@ -19,7 +20,12 @@ namespace ProceduralClimbing
         Vector3 rh, lh, rf, lf;
         Transform helper;
         bool isMirror;
+        bool isLeft;
+        Vector3 prevMovDir;
+        List<IKStates> ikStates = new List<IKStates>();
 
+        float delta;
+        public float lerpSpeed = 1.0f;
         public float wallOffset = 0f;
 
         public void Init(FreeClimb c, Transform helper)
@@ -31,20 +37,41 @@ namespace ProceduralClimbing
 
         public void CreatePositions(Vector3 origin, Vector3 moveDir, bool isMid)
         {
+            delta = Time.deltaTime;
             HandleAnim(moveDir, isMid);
 
             IKSnapshot ik = CreateSnapShot(origin);
             CopySnapshot(ref current, ik);
 
-            UpdateIKPosition(AvatarIKGoal.LeftHand, current.lh);
-            UpdateIKPosition(AvatarIKGoal.RightHand, current.rh);
-            UpdateIKPosition(AvatarIKGoal.LeftFoot, current.lf);
-            UpdateIKPosition(AvatarIKGoal.RightFoot, current.rf);
+            if(!isMid)
+            {
+                UpdateGoals(moveDir);
+                prevMovDir = moveDir;
+            }
+            else
+            {
+                UpdateGoals(prevMovDir);
+            }
+
+            SetIKPosition(isMid, goals.lh, current.lh, AvatarIKGoal.LeftHand);
+            SetIKPosition(isMid, goals.rh, current.rh, AvatarIKGoal.RightHand);
+            SetIKPosition(isMid, goals.lf, current.lf, AvatarIKGoal.LeftFoot);
+            SetIKPosition(isMid, goals.rf, current.rf, AvatarIKGoal.RightFoot);
 
             UpdateIKWeight(AvatarIKGoal.LeftHand, 1);
             UpdateIKWeight(AvatarIKGoal.RightHand, 1);
             UpdateIKWeight(AvatarIKGoal.LeftFoot, 1);
             UpdateIKWeight(AvatarIKGoal.RightFoot, 1);
+        }
+
+        void UpdateGoals(Vector3 moveDir)
+        {
+            isLeft = moveDir.x <= 0;
+
+            goals.lh = isLeft;
+            goals.rh = !isLeft;
+            goals.lf = isLeft;
+            goals.rf = !isLeft;
         }
 
         void HandleAnim(Vector3 moveDir, bool isMid)
@@ -125,6 +152,28 @@ namespace ProceduralClimbing
             to.rf = from.rf;
         }
 
+        void SetIKPosition(bool isMid, bool isTrue, Vector3 pos, AvatarIKGoal goal)
+        {
+            if(isMid)
+            {
+                if(isTrue)
+                {
+                    Vector3 p = GetPosActual(pos);
+                    UpdateIKPosition(goal, p);
+                }
+            }
+            else
+            {
+                if(!isTrue)
+                {
+                    Vector3 p = GetPosActual(pos);
+                    UpdateIKPosition(goal, p);
+                }
+            }
+
+
+        }
+
         public void UpdateIKPosition(AvatarIKGoal goal, Vector3 pos)
         {
             switch(goal)
@@ -165,6 +214,8 @@ namespace ProceduralClimbing
 
         private void OnAnimatorIK(int layerIndex)
         {
+            delta = Time.deltaTime;
+
             SetIKPos(AvatarIKGoal.LeftHand, lh, weight_lh);
             SetIKPos(AvatarIKGoal.RightHand, rh, weight_rh);
             SetIKPos(AvatarIKGoal.LeftFoot, lf, weight_lf);
@@ -173,18 +224,76 @@ namespace ProceduralClimbing
 
         void SetIKPos(AvatarIKGoal goal, Vector3 targetPos, float weight)
         {
-            anim.SetIKPositionWeight(goal, weight);
-            anim.SetIKPosition(goal, targetPos);
+            IKStates ikState = GetIKStates(goal);
+            if(ikState == null)
+            {
+                ikState = new IKStates();
+                ikState.goal = goal;
+                ikStates.Add(ikState);
+            }
+
+            if(weight == 0)
+            {
+                ikState.isSet = false;
+            }
+
+            if(!ikState.isSet)
+            {
+                ikState.position = GoalToBodyBones(goal).position;
+                ikState.isSet = true;
+            }
+
+            ikState.positionWeight = weight;
+            ikState.position = Vector3.Lerp(ikState.position, targetPos, delta * lerpSpeed);
+
+            anim.SetIKPositionWeight(goal, ikState.positionWeight);
+            anim.SetIKPosition(goal, ikState.position);
         }
 
-        // Use this for initialization
-        void Start () {
-            
+        Transform GoalToBodyBones(AvatarIKGoal goal)
+        {
+            switch (goal)
+            {
+                case AvatarIKGoal.LeftFoot:
+                    return anim.GetBoneTransform(HumanBodyBones.LeftFoot);
+                case AvatarIKGoal.RightFoot:
+                    return anim.GetBoneTransform(HumanBodyBones.RightFoot);
+                case AvatarIKGoal.LeftHand:
+                    return anim.GetBoneTransform(HumanBodyBones.LeftHand);
+                default:
+                case AvatarIKGoal.RightHand:
+                    return anim.GetBoneTransform(HumanBodyBones.RightHand);
+            }
         }
-        
-        // Update is called once per frame
-        void Update () {
-            
+
+        IKStates GetIKStates(AvatarIKGoal goal)
+        {
+            IKStates r = null;
+            foreach(IKStates i in ikStates)
+            {
+                if(i.goal == goal)
+                {
+                    r = i;
+                    break;
+                }
+
+            }
+
+            return r;
         }
+
+
+        class IKStates
+        {
+            public AvatarIKGoal goal;
+            public Vector3 position;
+            public float positionWeight;
+            public bool isSet = false;
+        }
+    }
+
+    public class IKGoals
+    {
+        public bool rh, lh, rf, lf;
     }
 }
